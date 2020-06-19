9cb6e85ad601dbcb579dda376e08c0e0852e0459
Gerald Adorza <gerald.adorza@gmail.com>
2020-06-19 15:12:50 +0800

    Deleted outdated class

--- Sources/Entities/PSStatement.swift
+++ Sources/Entities/PSStatement.swift
@@ -35,20 +35,3 @@ public class PSStatement: Mappable {
         PSMoney(amount: amountDecimal, currency: currency)
     }
 }
-
-public class PSWalletStatementOtherParty: Mappable {
-    
-    public var displayName: String!
-    public var walletId: Int!
-    public var accountNumber: String!
-    public var userId: Int!
-    
-    required public init?(map: Map) { }
-    
-    public func mapping(map: Map) {
-        displayName <- map["display_name"]
-        walletId <- map["wallet_id"]
-        accountNumber <- map["account_number"]
-        userId <- map["user_id"]
-    }
-}
