d15ff77ff16aacf37c88647d687498c7230fe36a
GERALD ADORZA <gerald.adorza@gmail.com>
2020-06-23 14:23:46 +0800

    Added currency filter parameter (#19)

--- Sources/Entities/PSStatementFilter.swift
+++ Sources/Entities/PSStatementFilter.swift
@@ -5,6 +5,7 @@ public class PSStatementFilter: PSBaseFilter {
     
     public var from: Date?
     public var to: Date?
+    public var currency: String?
     
     public func toJSON() -> [String : Any] {
         var parameters = super.toJSON()
@@ -13,6 +14,9 @@ public class PSStatementFilter: PSBaseFilter {
         }
         if let value = DateTransform().transformToJSON(to) {
             parameters["to"] = Int(value)
+        }
+        if let value = currency {
+            parameters["currency"] = value
         }
         return parameters
     }
