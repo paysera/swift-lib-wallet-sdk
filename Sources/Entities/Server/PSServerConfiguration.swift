0f24737be26d977ade557176e0f667f20fff2e43
Vytautas Gimbutas <v.gimbutas@paysera.com>
2020-07-03 00:57:22 +0300

    Removed bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp from PSServerConfiguration

--- Sources/Entities/Server/PSServerConfiguration.swift
+++ Sources/Entities/Server/PSServerConfiguration.swift
@@ -1,28 +1,21 @@
 import ObjectMapper
 
 public class PSServerConfiguration: Mappable {
-    
     public var minimumPasswordLength: Int
-    public var bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp: String
     
     required public init?(map: Map) {
         do {
             minimumPasswordLength = try map.value("minimum_password_length")
-            bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp = try map.value("bank_transfer_urgent_option_availability_beneficiary_account_regexp")
-            
         } catch {
             return nil
         }
     }
     
-    public init(minimumPasswordLength: Int,
-                bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp: String) {
+    public init(minimumPasswordLength: Int) {
         self.minimumPasswordLength = minimumPasswordLength
-        self.bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp = bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp
     }
     
     public func mapping(map: Map) {
-        minimumPasswordLength                                           <- map["minimum_password_length"]
-        bankTransferUrgentOptionAvailabilityBeneficiaryAccountRegexp    <- map["bank_transfer_urgent_option_availability_beneficiary_account_regexp"]
+        minimumPasswordLength <- map["minimum_password_length"]
     }
 }
