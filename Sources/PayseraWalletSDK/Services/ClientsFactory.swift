936198bc498684f08ec336b48aa4030db209be0f
Pattio <Pattio@users.noreply.github.com>
2021-01-20 18:44:06 +0200

    Remove PSTrustedSession

--- Sources/PayseraWalletSDK/Services/ClientsFactory.swift
+++ Sources/PayseraWalletSDK/Services/ClientsFactory.swift
@@ -79,10 +79,7 @@ public class ClientsFactory {
         return PublicWalletApiClient(session: session, logger: logger)
     }
     
-    private static func createSession(with interceptor: RequestInterceptor? = nil) -> PSTrustedSession {
-        return PSTrustedSession(
-            interceptor: interceptor,
-            hosts: ["wallet-api.paysera.com"]
-        )
+    private static func createSession(with interceptor: RequestInterceptor? = nil) -> Session {
+        Session(interceptor: interceptor)
     }
 }
