880b3e902bf3ccda6950758fa5a9686fe5e3f48b
Gerald Adorza <gerald.adorza@paysera.net>
2022-03-17 17:31:42 +0800

    tests

--- Sources/PayseraWalletSDK/Services/WalletAsyncClient/WalletAsyncClient.swift
+++ Sources/PayseraWalletSDK/Services/WalletAsyncClient/WalletAsyncClient.swift
@@ -565,4 +565,16 @@ public class WalletAsyncClient: BaseAsyncClient {
     ) -> Promise<PSApprovePartnerOAuthResponse> {
         doRequest(requestRouter: WalletApiRequestRouter.approvePartnerOAuthRequest(key: key))
     }
+
+    public func createPartnerOAuthRequestAny(
+        payload: PSCreatePartnerOAuthRequest
+    ) -> Promise<String> {
+        doRequest(requestRouter: WalletApiRequestRouter.createPartnerOAuthRequest(payload))
+    }
+    
+    public func approvePartnerOAusthRequestAny(
+        key: String
+    ) -> Promise<String> {
+        doRequest(requestRouter: WalletApiRequestRouter.approvePartnerOAuthRequest(key: key))
+    }
 }
