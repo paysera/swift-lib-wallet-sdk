c9431dcba0c783dd6f1c698a984e27993811c3ee
Gerald Adorza <gerald.adorza@paysera.net>
2022-03-24 14:05:55 +0800

    Added country of issue

--- Sources/PayseraWalletSDK/Entities/Identification/Request/PSIdentificationDocumentRequest.swift
+++ Sources/PayseraWalletSDK/Entities/Identification/Request/PSIdentificationDocumentRequest.swift
@@ -5,7 +5,7 @@ public class PSIdentificationDocumentRequest: Mappable {
     public var id: Int?
     public var type: String?
     public var personalNumber: String?
-    public var country: String?
+    public var countryOfIssue: String?
     public var dateOfBirth: String?   
     public var firstName: String?
     public var lastName: String?
@@ -22,7 +22,7 @@ public class PSIdentificationDocumentRequest: Mappable {
         id              <- map["id"]
         type            <- map["type"]
         personalNumber  <- map["personal_number"]
-        country         <- map["country"]
+        countryOfIssue  <- map["country_of_issue"]
         dateOfBirth     <- map["date_of_birth"]
         firstName       <- map["first_name"]
         lastName        <- map["last_name"]
