0a96d86fe576d44a7be59c02d43be5c3cd6bc555
Gerald Adorza <gerald.adorza@paysera.net>
2022-05-11 17:14:06 +0800

    Fixed status

--- Sources/PayseraWalletSDK/Entities/EasyPay/PSEasyPayTransferFilter.swift
+++ Sources/PayseraWalletSDK/Entities/EasyPay/PSEasyPayTransferFilter.swift
@@ -2,12 +2,12 @@ import ObjectMapper
 import PayseraCommonSDK
 
 public final class PSEasyPayTransferFilter: PSBaseFilter {
-    public var status: String?
+    public var status: PSEasyPayTransferStatus?
     public var beneficiaryUserID: String?
 
     public override func mapping(map: Map) {
         super.mapping(map: map)
-        status              <- map["status"]
+        status              <- (map["status"], PSEasyPayTransferStatus.Transform())
         beneficiaryUserID   <- map["beneficiary_user_id"]
     }
 }
