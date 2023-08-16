5e697c6ddb969d5b8a873b69af0188438e6a4c4a
Pavlo Kapinos <pavlo.kapinos@paysera.net>
2023-08-16 20:51:22 +0300

    Remove excessive error fields.

--- Sources/PayseraWalletSDK/Entities/Transfers/PSEvpAccount.swift
+++ Sources/PayseraWalletSDK/Entities/Transfers/PSEvpAccount.swift
@@ -2,14 +2,10 @@ import ObjectMapper
 
 public class PSEvpAccount: Mappable {
     public var accountNumber: String?
-    public var error: String?
-    public var errorDescription: String?
     
     required public init?(map: Map) {}
     
     public func mapping(map: Map) {
-        accountNumber    <- map["account_number"]
-        error            <- map["error"]
-        errorDescription <- map["error_description"]
+        accountNumber <- map["account_number"]
     }
 }
